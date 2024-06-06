ed98ea85a46aedad98305fa81baaad0bef05c2b6
xuzelong <xuzelong@meizu.com>
2024-06-06 10:45:24 +0800

    📦 NEW: 协同文档基建（基本的 websocket 通信）

---
+++ server/websocket.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 })
+const connections = [] // 存储连接的数组
+
+console.log('\x1b[32mWebSocket启动成功，端口号为8080\x1b[0m')
+
+const sentToAllConnections = (message) => {
+  connections.forEach((connection) => {
+    connection.send(message)
+  })
+}
+
+wss.on('connection', function connection(ws) {
+  console.log('有新的连接')
+  connections.push(ws)
+  ws.on('message', function incoming(message) {
+    console.log('\x1b[32m---收到客户端消息---\x1b[0m: %s', message)
+    sentToAllConnections(message)
+  })
+
+  ws.on('close', function disconnect() {
+    console.log('websocket断开连接')
+    // 在连接关闭时从数组中移除连接对象
+    const index = connections.indexOf(ws)
+    if (index !== -1) {
+      connections.splice(index, 1)
+    }
+  })
+})
---
+++ src/app/websocket/components/pageClient.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+
+const WebScoetPage = () => {
+  const socketRef = useRef<null | WebSocket>(null)
+  const inputRef = useRef<null | HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    const Socket = new WebSocket('ws://localhost:8080')
+    socketRef.current = Socket
+
+    Socket.onopen = () => {
+      console.log('\x1b[32m---WebSocket 连接成功---\x1b[0m')
+      //发送消息给服务端
+      // Socket.send('Hello , server端')
+    }
+
+    //接收服务端消息的回调函数
+    Socket.onmessage = (event) => {
+      const message = event.data
+      console.log('\x1b[32m---收到服务器的消息---\x1b[0m', message)
+      // inputRef.current!.value = message
+
+      // //收到信息为Blob类型时
+      let result = message
+      if (message instanceof Blob) {
+        const reader = new FileReader()
+        reader.readAsText(message, 'UTF-8')
+        reader.onload = (e) => {
+          //字符串和json格式
+          console.log(reader.result)
+          result = JSON.parse(JSON.stringify(reader.result))
+          console.log('\x1b[32m---websocket收到blob:---\x1b[0m', result)
+          inputRef.current!.value = result
+        }
+      } else {
+        inputRef.current!.value = result
+      }
+    }
+    //连接关闭的回调函数
+    Socket.onclose = () => {
+      console.log('WebSocket 连接已经关闭')
+    }
+    //连接错误的回调函数
+    Socket.onerror = () => {
+      console.log('WebSocket 连接发生错误')
+    }
+  }, [])
+
+  const sendMessage = () => {
+    if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+      socketRef.current.send(inputRef.current!.value)
+      // inputRef.current!.value = ''
+    } else {
+      console.log('WebSocket 连接未建立或已关闭')
+    }
+  }
+
+  return (
+    <div className="text-center h-screen">
+      <div className=" mt-[50vh] translate-y-[-50%]">
+        <p>
+          websocket，请先执行
+          <span className=" text-blue-300">`npm run server`</span>
+          启动服务端
+        </p>
+        <p>并打开控制台查看发送情况</p>
+        <div className=" my-[20px]">
+          <textarea
+            className=" w-[500px] h-[200px] text-black p-[10px]"
+            ref={inputRef}
+            onChange={sendMessage}
+          />
+        </div>
+        {/* <button onClick={sendMessage}>发送信息给服务端</button> */}
+      </div>
+    </div>
+  )
+}
+
+export default WebScoetPage
